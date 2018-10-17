09a3db815be09f49e60514fa64ea77d38ad059c9
ClaudioLopes <claudionazarethl@gmail.com>
2018-10-17 08:57:31 -0300

    Adicionado o padrão Facede(Front Controller)

---
+++ src/java/controler/FrontController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author claudio
+ */
+public class FrontController extends HttpServlet{
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String usuario = request.getParameter("usuario");
+        Usuario actionObject = null;
+        if(usuario == null || usuario.equals("")){
+            response.sendRedirect("index.jsp");
+        }
+        actionObject = ActionFactory.create(usuario);
+        if(actionObject != null){
+            actionObject.execute(request, response);
+        }
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
